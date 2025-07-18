172ade616360d8a0b7554d63bfbbfe59e2bdf83d
ervin49 <ervinmarin49@gmail.com>
2025-07-18 13:30:34 +0300

    added script config

---
+++ script.sh
@@ -0,0 +1,11 @@
+#\/bin/bash
+cp -r ./hypr/ ~/.config/
+cp -r ./Thunar/ ~/.config/
+cp -r ./waybar ~/.config/
+cp -r ./kitty/ ~/.config/
+cp -r ./rofi ~/.config/
+cp -r ./zathura/ ~/.config/
+cp -r ./nvim ~/.config/
+sudo cp -r ./logid.cfg /etc/logid.cfg
+cp -r ./tmux.conf ~/.tmux.conf
+cp -r ./tmux.conf.local /.tmux.conf.local
